77c7e197aa33f0dd0360d1f815548d8f4b6432d4
gauravkarkal <49086008+gauravkarkal@users.noreply.github.com>
2019-03-31 10:30:16 +0530

    Add files via upload

---
+++ magic_square.py
@@ -0,0 +1,13 @@
+import sys
+
+def formingMagicSquare(s):
+    print(s)
+    return "done"
+
+if __name__ == "__main__":
+    s = []
+    for s_i in range(3):
+       s_t = [int(s_temp) for s_temp in input().strip().split(' ')]
+       s.append(s_t)
+    result = formingMagicSquare(s)
+    print(result)
